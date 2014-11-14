5af0536451ae32eb36c6479cce2d7e81ca3bab8b
Rinchin <damdinovr@gmail.com>
2014-11-15 03:39:10 +0600

    Initial commit with bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Testcalendar::Application.config.secret_key_base = '2c91ddb1045a95fd9eb97ff992a77c3a47dc550a5995c6b20eb8824285b68aabe40f9995b4be07b329277d9dc53aa1d7898599a90c3511f031f7933e3bff481c'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Testcalendar::Application.config.session_store :cookie_store, key: '_testcalendar_session'
